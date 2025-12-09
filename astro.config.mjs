00d2912ad0e4bb49f662a613ee2ffb9ce9de1078
Shane Orr <shane.j.orr@gmail.com>
2025-12-09 09:27:47 -0500

    update vercel caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yourdomain.com', // Update with your domain
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+  }),
   integrations: [tailwind(), sitemap(), mdx()],
   image: {
     remotePatterns: [{ protocol: "https" }],
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-      config: {
-        limitInputPixels: false,
-      }
-    },
   },
 });
